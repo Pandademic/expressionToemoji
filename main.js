7b1b42ba09e84a602f1aa96747891bc5edd7c20e
Pandademic <74566464+Pandademic@users.noreply.github.com>
2021-07-05 13:20:18 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -29,3 +29,41 @@ function speak(){
   synth.speak(utterThis);
 
 }
+function check(){
+  img=document.getElementById('captured_image');
+  classifier.classify(img,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+      console.error(error);
+
+    }
+    else{
+       console.log(results);
+       document.getElementById("result_emotion_name").innerHTML=results[0].label;
+       document.getElementById("result_emotion_name2").innerHTML=results[1].label;
+       prediction_1=results[0].label;
+       prediction_2=results[1].label;
+       speak();
+       if(results[0].label=="Happy"){
+           document.getElementById("update_emoji").innerHTML="&#128522;";//Happy 
+       }
+       if(results[0].label=="Angry"){ 
+        document.getElementById("update_emoji").innerHTML="&#128545;";//Angry
+       }
+       if(results[0].label=="Suprised"){
+        document.getElementById("update_emoji").innerHTML="&#128562;";//suprised
+       }
+       if(results[1].label=="Happy"){
+        document.getElementById("update_emoji2").innerHTML="&#128522;";//Happy    
+       }
+       if(results[1].label=="Angry"){
+        document.getElementById("update_emoji2").innerHTML="&#128545;";//Angry
+
+       }
+       if(results[1].label=="Suprised"){
+        document.getElementById("update_emoji2").innerHTML="&#128562;";//suprised
+       }
+      }
+     
+}
